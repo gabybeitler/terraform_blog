742ac17f80d97c6c25ebc7666566f1c9142720ad
gaby beitler <gabybeitler@gmail.com>
2021-05-20 19:16:52 -0400

    Add network security

---
+++ network_security/variables.tf
@@ -0,0 +1,19 @@
+variable "cluster_name" {
+  type = string
+}
+variable "subnet_name" {
+  type = string
+}
+variable "password" {
+  type      = string
+  sensitive = true
+}
+variable "endpoint" {
+  type = string
+}
+variable "user" {
+  type = string
+}
+variable "image_uri" {
+  type = string
+}
